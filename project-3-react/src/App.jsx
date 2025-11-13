3052d6036471d30fa9e779076d8190a45715021c
FelixR77 <felix.romero75@gmail.com>
2025-11-13 14:40:46 -0700

    nav component

--- project-3-react/src/App.jsx
+++ project-3-react/src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import React from 'react'; 
+import NavBar from './components/nav.jsx'
 
 
   const App = () => {
   return (
-    <h1>My Branch</h1>
+    <>
+      <NavBar key="nav"></NavBar>
+      <h1>My Branch</h1>
+    </>
   );
 };
+
 
 export default App;
