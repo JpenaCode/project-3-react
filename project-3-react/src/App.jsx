2435d4a6a7c9305074e7f7bb78019f80578b1a2c
FelixR77 <felix.romero75@gmail.com>
2025-11-17 10:10:10 -0700

    commit to pull

--- project-3-react/src/App.jsx
+++ project-3-react/src/App.jsx
@@ -1,22 +1,49 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
-import NavBar from 'project-3-react/src/components/nav.jsx'
+import NavBar from './components/nav.jsx' // **
+import BookCard from './components/Bookcard.jsx'
 
 
 
 
 // import statements above
 const App = () => {
-	const [title, setTitle] = useState('');
-	const [author, setAuthor] = useState('');
-  const [genre, setGenre] = useState('');
 
-  const [searchTerm, setSearchTerm] = useState(''); // this was not in the lesson, this seperates the search state from the name state
+  const [books, setBooks] = useState([]); // **
+	// const [title, setTitle] = useState('');
+	// const [author, setAuthor] = useState('');
+  // const [genre, setGenre] = useState('');
+
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch('http://localhost:3000/books ');
+      const JSONdata = await response.json();
+
+      setBooks(JSONdata.results); // make sure this is an array
+      console.log(books)
+    };
+
+    getData();
+  }, []);
+
+  const filteredBooks = books.filter(book =>
+  book.name.toLowerCase().includes(search.toLowerCase())
+  );  
+  // helps with the filtering process
+
+
+
+
+  // const [searchTerm, setSearchTerm] = useState(''); // this was not in the lesson, this seperates the search state from the name state
 
   const handleChange = (event) => {
   setSearchTerm(event.target.value); // i replaced the setName to setSearchTerm so only the input updates and not the <p>Starship Name: {name}</p>
 };
 
+
+// Below is the Starthip lab 
+// ----------------------------------------------------------------
 // const YOUR_API_KEY = '93926e8f19954ff8892185839241302'
 
 // const handleSubmit = async (event) => {
@@ -35,19 +62,20 @@ const App = () => {
   return (
     <>
       <NavBar key="nav"></NavBar>
-      <form onSubmit={handleSubmit}>
-        Search your book :
-        <input type="text" onChange={handleChange} value={searchTerm}/> {/* i added the value={searchTerm}, this makes it so the that when the user types it shows up in the input and only in the input*/}
-        <input type="submit" value="book search" />
-      </form>
-     
-      <h1>Book:</h1>
-      <p>Book Title: {title}</p>
-      <p>Book Author: {author}</p>
-      <p>Book Genre: {genre}</p>
+      <BookCard books={filteredBooks}></BookCard>
     </>
   );
 };
+
+      // <form onSubmit={handleSubmit}>
+      //           Search your book :
+      //               <input type="text" onChange={handleChange} value={searchTerm}/> {/* i added the value={searchTerm}, this makes it so the that when the user types it shows up in the input and only in the input*/}
+      //               <input type="submit" value="book search" />
+      //   </form>
+      //               <h1>Book:</h1>
+      //               <p>Book Title: {title}</p>
+      //               <p>Book Author: {author}</p>
+      //               <p>Book Genre: {genre}</p>
 
 
 export default App;
--- project-3-react/src/components/nav.jsx
+++ project-3-react/src/components/nav.jsx
@@ -1,5 +1,5 @@
 import './nav.css';
-import React from 'react'; 
+
 
 const NavBar = () => {
     return (
